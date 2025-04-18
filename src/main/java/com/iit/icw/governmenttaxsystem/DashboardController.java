4343fde31685faa52c8a647e44639bdea64d970c
Arosha Withanage <avwithanage2006@gmail.com>
2025-04-18 14:23:07 +0530

    feat: add Login Stage

---
+++ src/main/java/com/iit/icw/governmenttaxsystem/DashboardController.java
@@ -0,0 +1,4 @@
+package com.iit.icw.governmenttaxsystem;
+
+public class DashboardController {
+}
---
+++ src/main/java/com/iit/icw/governmenttaxsystem/LoginController.java
@@ -0,0 +1,30 @@
+package com.iit.icw.governmenttaxsystem;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class LoginController {
+    @FXML
+    private Rectangle bgRectangle;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    protected void handleLoginButton() throws IOException {
+        Stage previousStage = (Stage) bgRectangle.getScene().getWindow();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("dashboard-view.fxml")));
+        previousStage.setScene(new Scene(root,  600, 400));
+        previousStage.setTitle("Dashboard | Government Tax Department System");
+        previousStage.setMaximized(true);
+        previousStage.show();
+    }
+}
